ee715769e5d0c14af047bdd6b9e30752b0cb0232
[Soumyadip Majumder] <[soumya_dip@live.in]>
2022-12-30 01:53:04 +0530

    steps for deploying and testing Lambda

--- 09-serverless/test.py
+++ 09-serverless/test.py
@@ -4,8 +4,8 @@ data = {
     "url": "http://bit.ly/mlbookcamp-pants" #"5089843a-ecf8-4cb6-8f35-587f19125046.jpg", #
 }
 
-url = "http://localhost:8080/2015-03-31/functions/function/invocations"
-# url = "https://0a1v3fyo2m.execute-api.eu-west-1.amazonaws.com/test/predict"
+#url = "http://localhost:8080/2015-03-31/functions/function/invocations" # For docker
+url = <"invoke_url> + "/predict" #for lambda funtion
 
 results = requests.post(url, json=data).json()
 
